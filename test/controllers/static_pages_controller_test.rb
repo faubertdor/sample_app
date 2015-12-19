fcc2bb155e6d35b86d10e5ce05f8ddc9a55cd42a
Jean Faubert Dorancy <faubertdor@gmail.com>
2015-12-19 13:35:02 -0500

    Add test for full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,25 +9,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "#{@base}"
+    assert_select "title", full_title
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base}"
+    assert_select "title", full_title("Help")
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base}"
+    assert_select "title", full_title("About")
   end
   
    test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | #{@base}"
+    assert_select "title", full_title("Contact")
   end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  def setup
-    @base = "Ruby on Rails Tutorial Sample App"
-  end
-  
+
   test "should get new" do
     get :new
     assert_response :success
-    assert_select "title", "Sign up | #{@base}"
+    assert_select "title", full_title("Sign up")
   end
 
 end
