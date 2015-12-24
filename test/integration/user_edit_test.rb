118247e63aefbdc0a80d0817f6fce5fb3f2e650a
Jean Faubert Dorancy <faubertdor@gmail.com>
2015-12-24 12:33:34 -0500

    Adding test to verify admin can't be set trough the web and verify layout links

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -22,6 +22,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert session[:forwarding_url].nil?
     name = "Jean"
     email = "user@example.com"
     patch user_path(@user), user: { name:  name,
