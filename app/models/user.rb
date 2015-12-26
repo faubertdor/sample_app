43d9b6f876ce4579ddd58691ddf7eda5729523bc
Jean Faubert Dorancy <faubertdor@gmail.com>
2015-12-26 16:54:07 -0500

    Use update_columns in place of update_attr and avoid displaying users that are not activated

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,8 +42,9 @@ class User < ActiveRecord::Base
   
   # Activate user upon confirmation
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
+    #update_attribute(:activated,    true)
+    #update_attribute(:activated_at, Time.zone.now)
   end
   
   # Send activation email :)
@@ -54,8 +55,10 @@ class User < ActiveRecord::Base
   # Sets attributes for password reset
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now)
+    #update_attribute(:reset_digest, User.digest(reset_token))
+    #update_attribute(:reset_sent_at, Time.zone.now)
   end
   
   # Send password reset email
