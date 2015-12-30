a29c8489139b04369ad206835d3c7ed136600802
Jean Faubert Dorancy <faubertdor@gmail.com>
2015-12-30 07:18:47 -0500

    Add test for image upload and correct bug with amazon s3 bucket

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -6,6 +6,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     @user = users(:jean)
   end
   
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with no micropost
+    other_user = users(:tamara)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "Something here")
+    get root_path
+    assert_match "#{other_user.microposts.count} micropost", response.body
+  end
+     
   test "micropost interface" do
     log_in_as(@user)
     get root_path
@@ -15,22 +29,25 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       post microposts_path, micropost: { content: "" }
     end
     assert_select 'div#error_explanation'
+    assert_select 'input[type=file]'
     # A valid submission
     content = "This is an example people"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
     # Delete a post
-    # assert_select 'a', text: 'delete'
+    assert_select 'a', text: 'Delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
     # A different user
-    get user_path(users(:tamara))
+    get user_path(users(:michael))
     assert_select 'a', text: 'delete', count: 0
   end
 end
